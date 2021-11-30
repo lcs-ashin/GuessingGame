3a0b2d74236833845c403f94f1d99f1cb7aa88a3
lcs-ashin <ashin@lcs.on.ca>
2021-11-30 14:30:47 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Eunbi Shin on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
